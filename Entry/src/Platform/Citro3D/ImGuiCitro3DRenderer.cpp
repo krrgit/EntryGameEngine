ffe14dd49a42272bc744324340b1cf62294f4b1e
kevin <89359821+krrgit@users.noreply.github.com>
2024-04-16 23:52:22 -0700

    optimization: using citro2d to draw elements

--- Entry/src/Platform/Citro3D/ImGuiCitro3DRenderer.cpp
+++ Entry/src/Platform/Citro3D/ImGuiCitro3DRenderer.cpp
@@ -36,16 +36,6 @@ bool ImGui_ImplC3D_Init()
 	ImGui_ImplC3D_Data* bd = IM_NEW(ImGui_ImplC3D_Data)();
 	io.BackendRendererUserData = (void*)bd;
 
-<<<<<<< Updated upstream
-	// Setup Citro2D, textures and images
-	bd->m_subt3x = { 512, 256, 0.0f, 1.0f, 1.0f, 0.0f };
-	bd->m_Image = (C2D_Image){ &(bd->m_tex), &(bd->m_subt3x) };
-	C3D_TexInit(&bd->m_tex, 512, 256, GPU_RGBA8);
-	C3D_TexSetFilter(&bd->m_tex, GPU_LINEAR, GPU_LINEAR);
-	C3D_TexSetWrap(&bd->m_tex, GPU_REPEAT, GPU_REPEAT);
-
-=======
->>>>>>> Stashed changes
 	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
 	C2D_Prepare();
 
@@ -79,24 +69,8 @@ bool ImGui_ImplC3D_Shutdown()
 bool ImGui_ImplC3D_NewFrame()
 {
 	ImGui_ImplC3D_Data* bd = ImGui_ImplC3D_GetBackendData();
-	std::fill_n(bd->m_PixelBuffer, bd->m_Width * bd->m_Height, 0x00000000u);
 
-<<<<<<< Updated upstream
-	imgui_sw::paint_imgui(bd->m_PixelBuffer, bd->m_Width, bd->m_Height, bd->sw_options);
-
-	for (u32 x = 0; x < bd->m_Width; x++)
-	{
-		for (u32 y = 0; y < bd->m_Height; y++)
-		{
-			u32 dstPos = ((((y >> 3) * (512 >> 3) + (x >> 3)) << 6) + ((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) | ((x & 4) << 2) | ((y & 4) << 3))) * 4;
-			u32 srcPos = (y * bd->m_Width + x) * 4;
-			memcpy(&((u8*)bd->m_Image.tex->data)[dstPos], &((u8*)bd->m_PixelBuffer)[srcPos], 4);
-		}
-	}
-
-=======
 	imgui_sw::paint_imgui(bd->m_Width, bd->m_Height, bd->sw_options);
->>>>>>> Stashed changes
 	return true;
 }
 
