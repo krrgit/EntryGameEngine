860ed331b1a29ae649789dd573d2d740aa911606
kevin <89359821+krrgit@users.noreply.github.com>
2025-07-24 23:45:33 -0700

    removed old code

--- Entry/src/Entry/Renderer/PerspectiveCamera.cpp
+++ Entry/src/Entry/Renderer/PerspectiveCamera.cpp
@@ -5,32 +5,9 @@
 
 namespace Entry {
 
-	//C3D_FVec vec3_to_c3d_fvec(glm::vec3 v) {
-	//	C3D_FVec result = { 1.0f, v.z, v.y, v.x };
-	//	return result;
-	//}
-
 	PerspectiveCamera::PerspectiveCamera(float _left, float _right, float _bottom, float _top)
 	{
 		SetProjection(_left, _right, _bottom, _top);
-
-		//// Set View Matrix
-		//m_ViewMatrix.r[0].x = 2.0f / (_right - _left);
-		//m_ViewMatrix.r[1].y = 2.0f / (_top - _bottom);
-		//m_ViewMatrix.r[2].z = -1;
-		//m_ViewMatrix.r[3].x = (_right + _left) / (_top - _bottom);
-		//m_ViewMatrix.r[3].y = (_top + _bottom) / (_top - _bottom);
-
-
-		//// Calculate ViewProjection & Projection
-		// Mtx_PerspTilt(&m_ProjectionMatrix, C3D_AngleFromDegrees(80.0f), C3D_AspectRatioTop, 0.01f, 1000.0f, false);
-		//Mtx_Multiply(&m_ViewProjectionMatrix, &m_ProjectionMatrix, &m_ViewMatrix);
-
-		//// Calculate normals
-		//Mtx_Transpose(&m_ViewMatrix);
-		//right = glm::vec3(m_ViewMatrix.m[3], m_ViewMatrix.m[7], m_ViewMatrix.m[11]);
-		//up = glm::vec3(m_ViewMatrix.m[2], m_ViewMatrix.m[6], m_ViewMatrix.m[10]);
-		//forward = glm::vec3(-m_ViewMatrix.m[1], -m_ViewMatrix.m[5], -m_ViewMatrix.m[9]);
 	}
 
 
@@ -41,8 +18,6 @@ namespace Entry {
 			glm::vec3(0, 0, 1), // look direction
 			glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 		);
-
-		//m_ProjectionMatrix = glm::perspective(glm::radians(fov), 400.0f/240.0f, 0.01f, 1000.0f);
 
 		// TODO: Only use when platform is 3DS
 		// 3DS screens are sideways. See Mtx_PerspTilt() in mtx_persplit.c for details.
@@ -65,11 +40,6 @@ namespace Entry {
 			m_ProjectionMatrix[2][2] = -m_ProjectionMatrix[2][3] * near / (near - far);
 		}
 
-		/*m_ProjectionMatrix = {  0,		1.2f,	0,	0,
-								-0.7f,	0,		0,	0,
-								0,		0,		0,	0,
-								0,		0,		-1.0f,	0
-		};*/
 		
 		m_ProjectionMatrix = glm::transpose(m_ProjectionMatrix);
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
@@ -79,40 +49,11 @@ namespace Entry {
 		right = glm::normalize(glm::vec3(normalMatrix[0][0], normalMatrix[1][0], normalMatrix[2][0]));
 		up = glm::normalize(glm::vec3(normalMatrix[0][1], normalMatrix[1][1], normalMatrix[2][1]));
 		forward = glm::normalize(glm::vec3(-normalMatrix[0][2], -normalMatrix[1][2], -normalMatrix[2][2]));
-
-
-		//--------------
-	
-		// Set View Matrix
-		//m_ViewMatrix = glm::mat4(1.0f);
-		//m_ViewMatrix.r[0].x = 2.0f / (_right - _left);
-		//m_ViewMatrix.r[1].y = 2.0f / (_top - _bottom);
-		//m_ViewMatrix.r[2].z = -1;
-		//m_ViewMatrix.r[3].x = (_right + _left) / (_top - _bottom);
-		//m_ViewMatrix.r[3].y = (_top + _bottom) / (_top - _bottom);
-
-		//// Calculate ViewProjection & Projection
-		//Mtx_PerspTilt(&m_ProjectionMatrix, C3D_AngleFromDegrees(80.0f), C3D_AspectRatioTop, 0.01f, 1000.0f, false);
-		//Mtx_Multiply(&m_ViewProjectionMatrix, &m_ProjectionMatrix, &m_ViewMatrix);
-
-		//// Calculate normals
-		//Mtx_Transpose(&m_ViewMatrix);
-		//right = glm::vec3(m_ViewMatrix.m[3], m_ViewMatrix.m[7], m_ViewMatrix.m[11]);
-		//up = glm::vec3(m_ViewMatrix.m[2], m_ViewMatrix.m[6], m_ViewMatrix.m[10]);
-		//forward = glm::vec3(-m_ViewMatrix.m[1], -m_ViewMatrix.m[5], -m_ViewMatrix.m[9]);
 	}
 
 
 	void PerspectiveCamera::RecalculateViewMatrix()
 	{
-
-		// Apply rotation in ZYX order (or any consistent order you expect)
-		//glm::mat4 transform = glm::mat4(1.0f);
-		//transform = glm::rotate(transform, glm::radians(m_Rotation.x), glm::vec3(1,0,0));
-		//transform = glm::rotate(transform, glm::radians(m_Rotation.y), glm::vec3(0,1,0));
-		//transform = glm::rotate(transform, glm::radians(m_Rotation.z), glm::vec3(0,0,1));
-		//transform = glm::translate(transform, m_Position);
-
 		glm::mat4 transform =	glm::translate(glm::mat4(1.0f), m_Position) *
 								glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation.z), glm::vec3(0,0,1)) *
 								glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation.y), glm::vec3(0,1,0)) *
@@ -136,25 +77,5 @@ namespace Entry {
 			glm_in[1].x, glm_in[1].y, glm_in[1].z,
 			glm_in[2].x, glm_in[2].y, glm_in[2].z
 		);
-
-		/*C3D_Mtx transform;
-		Mtx_Identity(&transform);
-		Mtx_Translate(&transform, m_Position.x, m_Position.y, m_Position.z, true);
-		Mtx_Rotate(&transform, vec3_to_c3d_fvec(right), glm::radians(m_Rotation.x), true);
-		Mtx_RotateY(&transform, glm::radians(m_Rotation.y), true);
-		Mtx_RotateZ(&transform, glm::radians(m_Rotation.z), true);
-
-		Mtx_Inverse(&transform);
-		m_ViewMatrix = transform;
-
-		Mtx_Multiply(&m_ViewProjectionMatrix, &m_ProjectionMatrix, &m_ViewMatrix);
-
-		C3D_Mtx normals;
-		Mtx_Copy(&normals, &transform);
-		Mtx_Transpose(&normals);
-		right = glm::vec3(normals.m[3], normals.m[7], normals.m[11]);
-		up = glm::vec3(normals.m[2], normals.m[6], normals.m[10]);
-		forward = glm::vec3(-normals.m[1], -normals.m[5], -normals.m[9]);
-		*/
 	}
 }
