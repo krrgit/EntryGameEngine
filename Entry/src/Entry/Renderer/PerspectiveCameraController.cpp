6512bb0019b25e8922f6d584c2063c25ad338882
kevin <89359821+krrgit@users.noreply.github.com>
2025-08-26 01:34:49 -0700

    added fast camera movement to Viewport camera

--- Entry/src/Entry/Renderer/PerspectiveCameraController.cpp
+++ Entry/src/Entry/Renderer/PerspectiveCameraController.cpp
@@ -50,7 +50,9 @@ namespace Entry {
         glm::vec3 right = m_Camera.right;
         //forward.y = 0; // uncomment to only move laterally
         //right.y = 0; // uncomment to only move laterally
-        m_CamPos = m_CamPos + forward * (cp.y * m_CameraTranslationSpeed * ts) + (right * (cp.x * m_CameraTranslationSpeed * ts));
+        float speedMult = Input::GetButton(KeyCode::LeftShift) ? m_MouseSpeedMult : 1.0f;
+
+        m_CamPos = m_CamPos + forward * (cp.y * m_CameraTranslationSpeed * ts * speedMult) + (right * (cp.x * m_CameraTranslationSpeed * ts * speedMult));
         int LandR = (Input::GetButton(ET_PAD_R) ? 1 : 0) - (Input::GetButton(ET_PAD_L) ? 1 : 0);
         m_CamPos.y += LandR * m_CameraVertSpeed * ts;
 
--- Entry/src/Entry/Renderer/PerspectiveCameraController.h
+++ Entry/src/Entry/Renderer/PerspectiveCameraController.h
@@ -45,6 +45,7 @@ namespace Entry {
 		glm::vec2 m_RotMouseCurrentPos;
 		glm::vec4 m_CamStartRot = { 0.0f, 0.0f, 0.0f, 0.0f };
 		float m_MouseSpeed = 0.1125f;
+		float m_MouseSpeedMult = 2.5f;
 
 		float m_Slider3DState, m_Slider3DStatePrev;
 
